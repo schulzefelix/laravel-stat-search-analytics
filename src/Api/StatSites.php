509073cff57ed02db3ed1758667bc2f883dbfdf2
Felix <felix@schulze.co>
2016-11-08 18:21:48 +0100

    implemented sites methods

--- src/Api/StatSites.php
+++ src/Api/StatSites.php
@@ -6,6 +6,26 @@ use Illuminate\Support\Collection;
 
 class StatSites extends BaseStat
 {
+
+    public function all() : Collection
+    {
+        $start = 0;
+        $sites = collect();
+
+        do {
+            $response = $this->performQuery('sites/all', ['start' => $start, 'results' => 5000]);
+            $start += 5000;
+            $sites = $sites->merge($response['Result']);
+
+            if(!isset($response['nextpage'])) {
+                break;
+            }
+        } while ($response['resultsreturned'] < $response['totalresults']);
+
+        return $sites;
+
+    }
+
     public function list($project_id) : Collection
     {
         $response = $this->performQuery('sites/list', ['project_id' => $project_id]);
@@ -16,4 +36,43 @@ class StatSites extends BaseStat
 
         return collect($response['Result']);
     }
+
+    public function rankingDistributions($siteID, $fromDate, $toDate) : Collection
+    {
+        $response = $this->performQuery('sites/ranking_distributions', ['id' => $siteID, 'from_date' => $fromDate, 'to_date' => $toDate]);
+
+        $rankDistribution = collect($response['RankDistribution']);
+
+        if(isset($response['RankDistribution']['date'])) {
+            $rankDistribution = collect([$response['RankDistribution']]);
+        }
+
+        return $rankDistribution;
+
+    }
+
+    public function create($projectID, $url, $dropWWWprefix = true, $dropDirectories = true)
+    {
+        $response = $this->performQuery('sites/create', ['project_id' => $projectID, 'url' => $url, 'drop_www_prefix' => $dropWWWprefix, 'drop_directories' => $dropDirectories]);
+
+        return $response['Result'];
+    }
+
+    public function update($siteID, array $attributes = [])
+    {
+        $arguments = ['id' => $siteID] + $attributes;
+
+        $response = $this->performQuery('sites/update', $arguments);
+
+        return $response['Result'];
+
+    }
+
+    public function delete($siteID)
+    {
+        $response = $this->performQuery('sites/delete', ['id' => $siteID]);
+
+        return (int) $response['Result']['Id'];
+    }
+
 }
--- tests/Unit/StatTest.php
+++ tests/Unit/StatTest.php
@@ -145,6 +145,332 @@ class StatTest extends PHPUnit_Framework_TestCase
     }
 
 
+    /** @test */
+    public function it_can_fetch_all_sites()
+    {
+        $expectedArguments = [
+            'sites/all', ['start' => 0, 'results' => 5000]
+        ];
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'totalresults' => "50",
+                'resultsreturned' => "50",
+                'nextpage' => "/sites/all?start=50&format=json",
+                'Result' => [
+                    [
+                        'Id' => "1",
+                        'ProjectId' => "13",
+                        'FolderId' => "22",
+                        'FolderName' => "Blog",
+                        'Title' => "gawker.com",
+                        'Url' => "gawker.com",
+                        'Synced' => "N/A",
+                        'TotalKeywords' => "63",
+                        'CreatedAt' => "2011-01-25",
+                        'UpdatedAt' => "2011-01-25",
+                        'RequestUrl' => "/keywords/list?site_id=1&format=json",
+                    ],
+                    [
+                        'Id' => "2",
+                        'ProjectId' => "13",
+                        'FolderId' => "N/A",
+                        'FolderName' => "N/A",
+                        'Title' => "perezhilton.com",
+                        'Url' => "perezhilton.com",
+                        'Synced' => "1",
+                        'TotalKeywords' => "63",
+                        'CreatedAt' => "2011-01-25",
+                        'UpdatedAt' => "2011-01-25",
+                        'RequestUrl' => "/keywords/list?site_id=2&format=json",
+                    ],
+                ]
+            ]]);
 
+        $response = $this->stat->sites()->all();
+
+        $this->assertInstanceOf(Collection::class, $response);
+        $this->assertEquals(2, $response->count());
+    }
+
+    /** @test */
+    public function it_can_list_sites_for_a_project()
+    {
+        $expectedArguments = [
+            'sites/list', ['project_id' => 13]
+        ];
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'resultsreturned' => "2",
+                'responsecode' => "200",
+                'Result' => [
+                    [
+                        'Id' => "1",
+                        'FolderId' => "22",
+                        'FolderName' => "Blog",
+                        'Title' => "gawker.com",
+                        'Url' => "gawker.com",
+                        'Synced' => "N/A",
+                        'TotalKeywords' => "63",
+                        'CreatedAt' => "2011-01-25",
+                        'UpdatedAt' => "2011-01-25",
+                        'RequestUrl' => "/keywords/list?site_id=1&format=json",
+                    ],
+                    [
+                        'Id' => "2",
+                        'FolderId' => "N/A",
+                        'FolderName' => "N/A",
+                        'Title' => "perezhilton.com",
+                        'Url' => "perezhilton.com",
+                        'Synced' => "1",
+                        'TotalKeywords' => "63",
+                        'CreatedAt' => "2011-01-25",
+                        'UpdatedAt' => "2011-01-25",
+                        'RequestUrl' => "/keywords/list?site_id=2&format=json",
+                    ],
+                ]
+            ]]);
+
+        $response = $this->stat->sites()->list(13);
+
+        $this->assertInstanceOf(Collection::class, $response);
+        $this->assertEquals(2, $response->count());
+        $this->assertEquals('gawker.com', $response->first()['Title']);
+        $this->assertEquals(63, $response->first()['TotalKeywords']);
+    }
+
+    /** @test */
+    public function it_can_create_new_sites_for_a_project()
+    {
+        $expectedArguments = [
+            'sites/create', ['project_id' => 13, 'url' => 'http://google.com', 'drop_www_prefix' => true, 'drop_directories' => true]
+        ];
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                        'Id' => "146",
+                        'ProjectId' => "13",
+                        'Title' => "google.com",
+                        'Url' => "google.com",
+                        'DropWWWPrefix' => "true",
+                        'DropDirectories' => "true",
+                        'CreatedAt' => "2011-01-25",
+                ]
+            ]]);
+
+        $response = $this->stat->sites()->create(13, 'http://google.com');
+
+        $this->assertInternalType('array', $response);
+        $this->assertEquals(146, $response['Id']);
+        $this->assertEquals("google.com", $response['Title']);
+    }
+
+    /** @test */
+    public function it_can_update_a_site()
+    {
+        $expectedArguments = [
+            'sites/update', ['id' => 13, 'url' => 'http://google.com', 'title' => 'my site', 'drop_www_prefix' => true, 'drop_directories' => true]
+        ];
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                        'Id' => "146",
+                        'ProjectId' => "13",
+                        'Title' => "my site",
+                        'Url' => "google.com",
+                        'DropWWWPrefix' => "true",
+                        'DropDirectories' => "true",
+                        'CreatedAt' => "2011-01-25",
+                        'UpdatedAt' => "2011-01-25",
+                ]
+            ]]);
+
+        $response = $this->stat->sites()->update(13, [
+            'url' => 'http://google.com',
+            'title' => 'my site',
+            'drop_www_prefix' => true,
+            'drop_directories' => true,
+        ]);
+
+        $this->assertInternalType('array', $response);
+        $this->assertEquals(146, $response['Id']);
+        $this->assertEquals("my site", $response['Title']);
+    }
+
+    /** @test */
+    public function it_can_delete_a_site()
+    {
+        $expectedArguments = [
+            'sites/delete', ['id' => 13]
+        ];
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                    'Id' => "13",
+                ]
+            ]]);
+
+        $response = $this->stat->sites()->delete(13);
+
+        $this->assertInternalType('int', $response);
+        $this->assertEquals(13, $response);
+    }
+
+    /** @test */
+    public function it_can_pull_the_ranking_distributions()
+    {
+        $expectedArguments = [
+            'sites/ranking_distributions', ['id' => 13, 'from_date' => '2016-10-01' , 'to_date' => '2016-10-02']
+        ];
+
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'RankDistribution' => [
+                    [
+                        'date' => '2016-10-01',
+                        'Google' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "2",
+                            "Five" => "2",
+                            "SixToTen" => "6",
+                            "ElevenToTwenty" => "5",
+                            "TwentyOneToThirty" => "5",
+                            "ThirtyOneToForty" => "2",
+                            "FortyOneToFifty" => "0",
+                            "FiftyOneToHundred" => "3",
+                            "NonRanking" => "11",
+                        ],
+                        'GoogleBaseRank' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "3",
+                            "Five" => "1",
+                            "SixToTen" => "7",
+                            "ElevenToTwenty" => "5",
+                            "TwentyOneToThirty" => "4",
+                            "ThirtyOneToForty" => "2",
+                            "FortyOneToFifty" => "0",
+                            "FiftyOneToHundred" => "3",
+                            "NonRanking" => "11",
+                        ],
+                        'Yahoo' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "0",
+                            "Five" => "1",
+                            "SixToTen" => "3",
+                            "ElevenToTwenty" => "6",
+                            "TwentyOneToThirty" => "5",
+                            "ThirtyOneToForty" => "4",
+                            "FortyOneToFifty" => "1",
+                            "FiftyOneToHundred" => "0",
+                            "NonRanking" => "16",
+                        ],
+                        'Bing' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "0",
+                            "Five" => "1",
+                            "SixToTen" => "3",
+                            "ElevenToTwenty" => "6",
+                            "TwentyOneToThirty" => "5",
+                            "ThirtyOneToForty" => "4",
+                            "FortyOneToFifty" => "1",
+                            "FiftyOneToHundred" => "0",
+                            "NonRanking" => "16",
+                        ]
+                    ],
+                    [
+                        'date' => '2016-10-02',
+                        'Google' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "2",
+                            "Five" => "2",
+                            "SixToTen" => "6",
+                            "ElevenToTwenty" => "5",
+                            "TwentyOneToThirty" => "5",
+                            "ThirtyOneToForty" => "2",
+                            "FortyOneToFifty" => "0",
+                            "FiftyOneToHundred" => "3",
+                            "NonRanking" => "11",
+                        ],
+                        'GoogleBaseRank' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "3",
+                            "Five" => "1",
+                            "SixToTen" => "7",
+                            "ElevenToTwenty" => "5",
+                            "TwentyOneToThirty" => "4",
+                            "ThirtyOneToForty" => "2",
+                            "FortyOneToFifty" => "0",
+                            "FiftyOneToHundred" => "3",
+                            "NonRanking" => "11",
+                        ],
+                        'Yahoo' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "0",
+                            "Five" => "1",
+                            "SixToTen" => "3",
+                            "ElevenToTwenty" => "6",
+                            "TwentyOneToThirty" => "5",
+                            "ThirtyOneToForty" => "4",
+                            "FortyOneToFifty" => "1",
+                            "FiftyOneToHundred" => "0",
+                            "NonRanking" => "16",
+                        ],
+                        'Bing' => [
+                            "One" => "0",
+                            "Two" => "0",
+                            "Three" => "1",
+                            "Four" => "0",
+                            "Five" => "1",
+                            "SixToTen" => "3",
+                            "ElevenToTwenty" => "6",
+                            "TwentyOneToThirty" => "5",
+                            "ThirtyOneToForty" => "4",
+                            "FortyOneToFifty" => "1",
+                            "FiftyOneToHundred" => "0",
+                            "NonRanking" => "16",
+                        ]
+                    ],
+                ]
+            ]]);
+
+        $response = $this->stat->sites()->rankingDistributions(13, '2016-10-01', '2016-10-02');
+
+        $this->assertInstanceOf(Collection::class, $response);
+        $this->assertEquals(2, $response->count());
+        $this->assertEquals('2016-10-01', $response->first()['date']);
+        $this->assertEquals(12, count($response->first()['Google']));
+        $this->assertEquals(5, $response->first()['Google']['ElevenToTwenty']);
+
+    }
 
 }
