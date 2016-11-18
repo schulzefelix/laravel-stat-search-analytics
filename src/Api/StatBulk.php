c70f5535600b411aca7df2e40b83ba469b7456a7
Felix <felix@schulze.co>
2016-11-18 17:17:39 +0100

    bulk methods

--- src/Api/StatBulk.php
+++ src/Api/StatBulk.php
@@ -4,6 +4,7 @@ namespace SchulzeFelix\Stat\Api;
 
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use SchulzeFelix\Stat\Exceptions\ApiException;
 
 class StatBulk extends BaseStat
 {
@@ -26,11 +27,94 @@ class StatBulk extends BaseStat
             ];
 
         });
+    }
+
+    /**
+     * @param Carbon $date
+     * @param array|null $sites
+     * @param string $rankType
+     * @param array|null $engines
+     * @param bool $currentlyTrackedOnly
+     * @param bool $crawledKeywordsOnly
+     * @return int
+     */
+    public function ranks(Carbon $date, array $sites = null, $rankType = 'highest', $engines = ['google', 'yahoo', 'bing'], bool $currentlyTrackedOnly = false , bool $crawledKeywordsOnly = false)
+    {
+        $this->validateBulkDate($date);
+
+        $arguments['date'] = $date->toDateString();
+        $arguments['rank_type'] = $rankType;
+        $arguments['currently_tracked_only'] = $currentlyTrackedOnly;
+        $arguments['crawled_keywords_only'] = $crawledKeywordsOnly;
+
+        if( ! is_null($sites) && count($sites) > 0){
+            $arguments['site_id'] = implode(',', $sites);;
+        }
+        if( ! is_null($engines) && count($engines) > 0){
+            $arguments['engines'] = implode(',', $engines);
+        }
+        $response = $this->performQuery('bulk/ranks', $arguments);
+
+        return (int) $response['Result']['Id'];
+    }
+
+    public function status($bulkJobID)
+    {
+        $response = $this->performQuery('bulk/status', ['id' => $bulkJobID]);
+
+        $jobStatus = [];
+        $jobStatus['id'] = $response['Result']['Id'];
+        $jobStatus['job_type'] = $response['Result']['JobType'];
+        $jobStatus['format'] = $response['Result']['Format'];
+        $jobStatus['date'] = Carbon::parse($response['Result']['Date']);
+
+        $jobStatus['sites'] = collect();
+        if(isset($response['Result']['SiteId'])){
+            $jobStatus['sites'] = collect( explode(',', $response['Result']['SiteId']));
+        }
+        //Current Job Status (NotStarted,InProgress,Completed,Deleted,Failed)
+        $jobStatus['status'] = $response['Result']['Status'];
+        $jobStatus['url'] = $response['Result']['Url'] ?? null;
+        $jobStatus['stream_url'] = $response['Result']['StreamUrl'] ?? null;
+        $jobStatus['created_at'] = Carbon::parse($response['Result']['CreatedAt']);
+
+        return $jobStatus;
 
     }
 
+    public function delete($bulkJobID)
+    {
+        $response = $this->performQuery('bulk/delete', ['id' => $bulkJobID]);
 
+        return (int) $response['Result']['Id'];
+    }
 
+    public function siteRankingDistributions($date)
+    {
+        $this->validateBulkDate($date);
+
+        $response = $this->performQuery('bulk/site_ranking_distributions', ['date' => $date->toDateString()]);
+        return (int) $response['Result']['Id'];
+    }
+
+    public function tagRankingDistributions($date)
+    {
+        $this->validateBulkDate($date);
+
+        $response = $this->performQuery('bulk/tag_ranking_distributions', ['date' => $date->toDateString()]);
+        return (int) $response['Result']['Id'];
+    }
+
+    /**
+     * @param Carbon $date
+     * @throws ApiException
+     */
+    private function validateBulkDate(Carbon $date)
+    {
+        if ($date->isSameDay(Carbon::now()) or $date->isFuture()) {
+            throw ApiException::resultError('You cannot create bulk jobs for the current date or dates in the future.');
+        }
+    }
 
 
 
--- tests/Unit/BulkTest.php
+++ tests/Unit/BulkTest.php
@@ -87,11 +87,175 @@ class BulkTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('ranks', $response->first()['job_type']);
         $this->assertEquals('NotStarted', $response->first()['status']);
 
-
-
-
     }
 
 
+    /** @test */
+    public function it_can_create_bulk_exports_jobs_for_ranks()
+    {
+        $expectedArguments = [
+            'bulk/ranks', ['date' => '2016-11-08', 'site_id' => '1,2', 'rank_type' => 'highest', 'crawled_keywords_only' => true, 'currently_tracked_only' => false]
+        ];
+
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                    'Id' => '1',
+                ]
+            ]]);
+
+        $response = $this->stat->bulk()->ranks(Carbon::create(2016, 11, 8), [1,2], 'highest', null, false, true);
+        $this->assertEquals(1, $response);
+    }
+
+    /** @test */
+    public function it_throw_an_exception_if_i_want_create_a_bulk_job_for_today()
+    {
+        $this->setExpectedException(ApiException::class);
+
+        $response = $this->stat->bulk()->ranks(Carbon::now());
+
+    }
+
+    /** @test */
+    public function it_throw_an_exception_if_i_want_create_a_bulk_job_for_the_future()
+    {
+        $this->setExpectedException(ApiException::class);
+
+        $response = $this->stat->bulk()->ranks(Carbon::now()->addWeek());
+    }
+
+
+    /** @test */
+    public function it_can_get_bulk_exports_jobs_status()
+    {
+        $expectedArguments = [
+            'bulk/status', ['id' => 1]
+        ];
+
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                    'Id' => '1',
+                    'JobType' => 'ranks',
+                    'Format' => 'json',
+                    'Date' => '2011-04-15',
+                    'SiteId' => '1,5,10',
+                    'Status' => 'InProgress',
+                    'Url' => null,
+                    'CreatedAt' => '2011-05-03',
+                ]
+            ]]);
+
+        $response = $this->stat->bulk()->status(1);
+        $this->assertInternalType('array', $response);
+        $this->assertEquals(9, count($response));
+
+        $this->assertArrayHasKey('id', $response);
+        $this->assertArrayHasKey('job_type', $response);
+        $this->assertArrayHasKey('format', $response);
+        $this->assertArrayHasKey('date', $response);
+        $this->assertArrayHasKey('sites', $response);
+        $this->assertArrayHasKey('url', $response);
+        $this->assertArrayHasKey('stream_url', $response);
+        $this->assertArrayHasKey('created_at', $response);
+        $this->assertInstanceOf(Collection::class, $response['sites']);
+
+        $this->assertEquals(1, $response['id']);
+        $this->assertEquals('ranks', $response['job_type']);
+        $this->assertEquals('json', $response['format']);
+        $this->assertEquals('InProgress', $response['status']);
+        $this->assertEquals(null, $response['url']);
+        $this->assertEquals(null, $response['stream_url']);
+        $this->assertEquals(3, $response['sites']->count());
+        $this->assertInstanceOf(Carbon::class, $response['created_at']);
+        $this->assertEquals('2011-05-03', $response['created_at']->toDateString());
+        $this->assertInstanceOf(Carbon::class, $response['date']);
+        $this->assertEquals('2011-04-15', $response['date']->toDateString());
+    }
+
+    /** @test */
+    public function it_can_delete_a_bulk_exports_jobs()
+    {
+        $expectedArguments = [
+            'bulk/delete', ['id' => 1]
+        ];
+
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                    'Id' => '1'
+                ]
+            ]]);
+
+        $response = $this->stat->bulk()->delete(1);
+        $this->assertInternalType('int', $response);
+        $this->assertEquals(1, $response);
+    }
+
+    /** @test */
+    public function it_can_create_bulk_exports_jobs_for_site_ranking_distributions()
+    {
+        $expectedArguments = [
+            'bulk/site_ranking_distributions', ['date' => '2016-11-08']
+        ];
+
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                    'Id' => '1',
+                ]
+            ]]);
+
+        $response = $this->stat->bulk()->siteRankingDistributions(Carbon::create(2016, 11, 8));
+        $this->assertEquals(1, $response);
+    }
+
+    /** @test */
+    public function it_throw_an_exception_if_i_want_create_a_bulk_job_site_ranking_distributions_for_the_future()
+    {
+        $this->setExpectedException(ApiException::class);
+        $response = $this->stat->bulk()->siteRankingDistributions(Carbon::now()->addMonth());
+    }
+
+    /** @test */
+    public function it_can_create_bulk_exports_jobs_for_tag_ranking_distributions()
+    {
+        $expectedArguments = [
+            'bulk/tag_ranking_distributions', ['date' => '2016-11-08']
+        ];
+
+        $this->statClient
+            ->shouldReceive('performQuery')->withArgs($expectedArguments)
+            ->once()
+            ->andReturn(['Response' => [
+                'responsecode' => "200",
+                'Result' => [
+                    'Id' => '1',
+                ]
+            ]]);
+
+        $response = $this->stat->bulk()->tagRankingDistributions(Carbon::create(2016, 11, 8));
+        $this->assertEquals(1, $response);
+    }
+
+    /** @test */
+    public function it_throw_an_exception_if_i_want_create_a_bulk_job_tag_ranking_distributions_for_the_future()
+    {
+        $this->setExpectedException(ApiException::class);
+        $response = $this->stat->bulk()->siteRankingDistributions(Carbon::now()->addDay());
+    }
 
 }
